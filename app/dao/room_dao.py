fa08d893a98e95f5f38ccda35a43eccaa8c6ffc7
maxkondaurov <maxkondaurov2@yandex.ru>
2025-06-26 17:06:31 +0300

    Немного доработана версия, плюс добавил кэш (Redis)

--- app/dao/room_dao.py
+++ app/dao/room_dao.py
@@ -1,7 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, delete
 from app.models.rooms import Rooms
-from app.schemas.room_schemas import RoomCreate
+from app.schemas.room_schemas import RoomCreate, RoomRead
+from app.services.redis_cache import redis_client
+import json
+from fastapi.encoders import jsonable_encoder
 
 class RoomDAO:
     @staticmethod
@@ -10,13 +13,37 @@ class RoomDAO:
         session.add(room)
         await session.commit()
         await session.refresh(room)
+        await redis_client.delete("rooms_all")
         return room
 
 
     @staticmethod
-    async def get_all(session: AsyncSession) -> list[Rooms]:
+    async def get_all(session: AsyncSession) -> list[RoomRead]:
+        cached = await redis_client.get("rooms_all")
+        if cached:
+            print("Получаем из кеша")
+            rooms_data = json.loads(cached)
+            return [RoomRead(**room) for room in rooms_data]
+
+        print("Получаем из базы")
         result = await session.execute(select(Rooms))
-        return result.scalars().all()
+        rooms = result.scalars().all()
+
+        await redis_client.set("rooms_all", json.dumps([
+            {
+                "id": r.id,
+                "name": r.name,
+                "price": r.price,
+                "description": r.description,
+                "is_available": r.is_available
+            }
+            for r in rooms
+        ]), ex=60)
+
+        rooms_serialized = jsonable_encoder([RoomRead.model_validate(r) for r in rooms])
+        await redis_client.set("rooms_all", json.dumps(rooms_serialized), ex=60)
+
+        return [RoomRead.model_validate(r) for r in rooms]
 
     @staticmethod
     async def delete_room(session: AsyncSession, room_id: int) -> bool:
@@ -31,4 +58,8 @@ class RoomDAO:
 
         await session.delete(room)
         await session.commit()
+
+        await redis_client.delete("rooms_all")
+
         return True
+
--- app/schemas/room_schemas.py
+++ app/schemas/room_schemas.py
@@ -1,13 +1,15 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, conint
 from typing import Optional
 
 class RoomCreate(BaseModel):
     name: str = Field(max_length=100, min_length=2)
-    price: int = Field(conint=0)
+    price: int = conint(ge=0)
     description: Optional[str] = Field(max_length=255)
+    is_available: bool
 
 class RoomRead(RoomCreate):
     id: int
+
 
     class Config:
         from_attributes = True
